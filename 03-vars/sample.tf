b4db7cfe625327ecc10f97863a1c2b6bc741cab7
Welcome <jakdevops@gmail.com>
2023-08-03 02:51:07 -0700

    terraform variable declaration

--- 03-vars/sample.tf
+++ 03-vars/sample.tf
@@ -5,3 +5,48 @@ variable "fruit_name"{
 output "furit_name"{
     value = var.fruit_name
     }
+
+#list varaiables
+ variable "fruits"{
+     default = [
+      "apple"
+      "guva"
+ ]
+     default = [ "apple","guva" ]  // list of variable
+     }
+
+   #map variable
+   variable "fruit_stock"{
+     default = {
+       apple = 100
+       guva = 210
+   }
+  }
+
+  #map variable, map of maps
+  variable "fruit_stock_with_price" {
+    default = {
+      apple = {
+         stock =  100
+         price = 1
+       }
+       guva = {
+         stock = 202
+         price = 1
+        }
+       }
+      }
+
+  #how to access a list variable, list index start from zero
+   output "fruits_first" {
+      value = var.fruits[0]
+      }
+
+      output "fruits_second"{
+         value = var.fruits[1]
+      }
+   # how to access map variable
+
+   output "fruit_stock_apple"{
+    value = var.fruit_stock["apple"]
+   }
