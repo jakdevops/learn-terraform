7e46ad41b6096f5c266c47e0d5699295ee2a1b4c
Welcome <jakdevops@gmail.com>
2023-08-05 02:06:11 -0700

    terraform module

--- 07-module/local-module/data.tf
+++ 07-module/local-module/data.tf
@@ -1,8 +1,16 @@
-data "aws_ami_ids" "ami" {
-   name_regex      = "centos"
+data "aws_ami" "ami" {
+   most recent = true
+   name_regex      = "Centos-8-DevOps-Practice"
    owners          = [ "621241008916"]
    }
 
    output "ami" {
-     value = data.aws_ami_ids.ami
+     value = data.aws_ami.ami
      }
+
+   resource "aws_instance" "instance" {
+   ami     = data.aws_ami.ami.id
+   instance_type = "t3.small"
+   vpc_security_group_ids = [ "sg-0da802b753384d70d" ]
+   }
+
